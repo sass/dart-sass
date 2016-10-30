8ff9150cf8f499098ba5d11931d32e88947d14ad
Natalie Weizenbaum <nex342@gmail.com>
2016-10-30 12:54:24 -0700

    Add Travis support.
    Currently this only runs --expanded tests, because they're the only
    tests that have been triaged.

---
+++ tool/assert-formatted.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+# Copyright 2016 Google Inc. Use of this source code is governed by an MIT-style
+# license that can be found in the LICENSE file or at
+# https://opensource.org/licenses/MIT.
+
+unformatted=`pub run dart_style:format -n bin/ lib/ tool/`
+if [[ -z "$unformatted" ]]; then
+    exit 0
+else
+    echo "Files are unformatted:"
+    echo "$unformatted"
+    exit 1
+fi
