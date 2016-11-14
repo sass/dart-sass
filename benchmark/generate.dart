5de78148c898d5557d8bf462c10c2c090e278288
Natalie Weizenbaum <nweiz@google.com>
2016-11-14 15:20:32 -0800

    Format benchmark/generate.dart.

--- benchmark/generate.dart
+++ benchmark/generate.dart
@@ -12,14 +12,24 @@ main() async {
   if (!await sources.exists()) await sources.create();
 
   await writeNTimes("${sources.path}/small_plain.scss", ".foo {a: b}", 4);
-  await writeNTimes("${sources.path}/large_plain.scss", ".foo {a: b}", math.pow(2, 17));
-  await writeNTimes("${sources.path}/preceding_sparse_extend.scss", ".foo {a: b}", math.pow(2, 17), header: '.x {@extend .y}', footer: '.y {a: b}');
-  await writeNTimes("${sources.path}/following_sparse_extend.scss", ".foo {a: b}", math.pow(2, 17), header: '.y {a: b}', footer: '.x {@extend .y}');
-  await writeNTimes("${sources.path}/preceding_dense_extend.scss", ".foo {a: b}", math.pow(2, 17), header: '.bar {@extend .foo}');
-  await writeNTimes("${sources.path}/following_dense_extend.scss", ".foo {a: b}", math.pow(2, 17), footer: '.bar {@extend .foo}');
+  await writeNTimes(
+      "${sources.path}/large_plain.scss", ".foo {a: b}", math.pow(2, 17));
+  await writeNTimes("${sources.path}/preceding_sparse_extend.scss",
+      ".foo {a: b}", math.pow(2, 17),
+      header: '.x {@extend .y}', footer: '.y {a: b}');
+  await writeNTimes("${sources.path}/following_sparse_extend.scss",
+      ".foo {a: b}", math.pow(2, 17),
+      header: '.y {a: b}', footer: '.x {@extend .y}');
+  await writeNTimes("${sources.path}/preceding_dense_extend.scss",
+      ".foo {a: b}", math.pow(2, 17),
+      header: '.bar {@extend .foo}');
+  await writeNTimes("${sources.path}/following_dense_extend.scss",
+      ".foo {a: b}", math.pow(2, 17),
+      footer: '.bar {@extend .foo}');
 }
 
-Future writeNTimes(String path, String text, int times, {String header, String footer}) async {
+Future writeNTimes(String path, String text, int times,
+    {String header, String footer}) async {
   print("Generating $path...");
   var file = new File(path).openWrite();
   if (header != null) file.writeln(header);
