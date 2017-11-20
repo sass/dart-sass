536ea75db89784daac9fd5f474dfc86684dbce70
Natalie Weizenbaum <nweiz@google.com>
2017-11-20 14:30:42 -0800

    Compile asynchronous code to synchronous (#194)
    See #9

---
+++ test/synchronize_test.dart
@@ -0,0 +1,27 @@
+// Copyright 2017 Google Inc. Use of this source code is governed by an
+// MIT-style license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:crypto/crypto.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test("synchronized files are up-to-date", () {
+    ({
+      'lib/src/visitor/async_evaluate.dart': 'lib/src/visitor/evaluate.dart',
+      'lib/src/async_environment.dart': 'lib/src/environment.dart'
+    }).forEach((sourcePath, targetPath) {
+      var source = new File(sourcePath).readAsStringSync();
+      var target = new File(targetPath).readAsStringSync();
+
+      var hash = sha1.convert(UTF8.encode(source));
+      if (!target.contains("Checksum: $hash")) {
+        fail("$targetPath is out-of-date.\n"
+            "Run pub run grinder to update it.");
+      }
+    });
+  });
+}
