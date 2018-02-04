b18035ec82c097e5f1f90f0f06a6f926ab7d706e
Natalie Weizenbaum <nweiz@google.com>
2018-02-03 18:47:56 -0800

    Improve deploy script logging

--- tool/deploy.sh
+++ tool/deploy.sh
@@ -5,16 +5,34 @@
 
 openssl aes-256-cbc -K $encrypted_d18df560dfb2_key -iv $encrypted_d18df560dfb2_iv \
         -in tool/encrypted/npmrc.enc -out ~/.npmrc -d
-npm publish build/npm
-npm publish build/npm-old
-
 mkdir -p ~/.pub-cache
 openssl aes-256-cbc -K $encrypted_d18df560dfb2_key -iv $encrypted_d18df560dfb2_iv \
         -in tool/encrypted/pub-credentials.json.enc -out ~/.pub-cache/credentials.json -d
-pub lish
-
 openssl aes-256-cbc -K $encrypted_d18df560dfb2_key -iv $encrypted_d18df560dfb2_iv \
         -in tool/encrypted/git-credentials.enc -out ~/.git-credentials -d
+
+function travis_cmd() {
+  echo "\$ $@"
+  "$@"
+}
+
+travis_fold() {
+  local action=$1
+  local name=$2
+  echo -en "travis_fold:${action}:${name}\r"
+}
+
+travis_fold start npm
+travis_cmd npm publish build/npm
+travis_cmd npm publish build/npm-old
+travis_fold end npm
+
+travis_fold start pub
+travis_cmd pub lish
+travis_fold end pub
+
+travis_fold start homebrew
 git config --local user.name "Natalie Weizenbaum"
 git config --local user.email "nweiz@google.com"
-pub run grinder update-homebrew
+travis_cmd pub run grinder update-homebrew
+travis_fold end homebrew
