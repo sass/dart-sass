468e3b0270706b41e48323b2b3870dc5fc3bba1e
Natalie Weizenbaum <nweiz@google.com>
2018-07-26 16:21:02 -0700

    Normalize escapes in identifiers (#427)
    Closes #425
    See sass/sass#1542

--- lib/src/parse/parser.dart
+++ lib/src/parse/parser.dart
@@ -112,8 +112,9 @@ abstract class Parser {
   /// `px-2px`.
   @protected
   String identifier({bool unit: false}) {
-    // NOTE: this logic is largely duplicated in ScssParser.identifier.
-    // Most changes here should be mirrored there.
+    // NOTE: this logic is largely duplicated in
+    // StylesheetParser._interpolatedIdentifier. Most changes here should be
+    // mirrored there.
 
     var text = new StringBuffer();
     while (scanner.scanChar($dash)) {
@@ -126,7 +127,7 @@ abstract class Parser {
     } else if (isNameStart(first)) {
       text.writeCharCode(scanner.readChar());
     } else if (first == $backslash) {
-      text.write(escape());
+      text.write(escape(identifierStart: true));
     } else {
       scanner.error("Expected identifier.");
     }
@@ -221,7 +222,7 @@ abstract class Parser {
       var next = scanner.peekChar();
       switch (next) {
         case $backslash:
-          buffer.write(escape());
+          buffer.write(escape(identifierStart: true));
           wroteNewline = false;
           break;
 
@@ -364,16 +365,18 @@ abstract class Parser {
   // ## Characters
 
   /// Consumes an escape sequence and returns the text that defines it.
+  ///
+  /// If [identifierStart] is true, this normalizes the escape sequence as
+  /// though it were at the beginning of an identifier.
   @protected
-  String escape() {
+  String escape({bool identifierStart: false}) {
     // See https://drafts.csswg.org/css-syntax-3/#consume-escaped-code-point.
 
-    var buffer = new StringBuffer();
     scanner.expectChar($backslash);
-    buffer.writeCharCode($backslash);
+    var value = 0;
     var first = scanner.peekChar();
     if (first == null) {
-      // Do nothing.
+      return "";
     } else if (isNewline(first)) {
       scanner.error("Expected escape sequence.");
       return null;
@@ -381,17 +384,26 @@ abstract class Parser {
       for (var i = 0; i < 6; i++) {
         var next = scanner.peekChar();
         if (next == null || !isHex(next)) break;
-        buffer.writeCharCode(scanner.readChar());
+        value *= 16;
+        value += asHex(scanner.readChar());
       }
 
-      if (isWhitespace(scanner.peekChar())) {
-        buffer.writeCharCode(scanner.readChar());
-      }
+      scanCharIf(isWhitespace);
     } else {
-      buffer.writeCharCode(scanner.readChar());
+      value = scanner.readChar();
     }
 
-    return buffer.toString();
+    if (identifierStart ? isNameStart(value) : isName(value)) {
+      return new String.fromCharCode(value);
+    } else if (value < 0x1F || value == 0x7F) {
+      var buffer = new StringBuffer()..writeCharCode($backslash);
+      if (value > 0xF) buffer.writeCharCode(hexCharFor(value >> 4));
+      buffer.writeCharCode(hexCharFor(value & 0xF));
+      buffer.writeCharCode($space);
+      return buffer.toString();
+    } else {
+      return new String.fromCharCodes([$backslash, value]);
+    }
   }
 
   /// Consumes an escape sequence and returns the character it represents.
