19767a73d8ae57af6d7da8dc42f3e5abc577999c
Progracientist <hi.progracientist@outlook.com>
2018-09-25 17:46:03 +0100

    Changed prettify into new variable destinationName and output to buffer

--- lib/src/executable/compile_stylesheet.dart
+++ lib/src/executable/compile_stylesheet.dart
@@ -79,8 +79,8 @@ Future compileStylesheet(ExecutableOptions options, StylesheetGraph graph,
   if (options.color) buffer.write('\u001b[32m');
 
   var sourceName = source == null ? 'stdin' : p.prettyUri(p.toUri(source));
-  destination = p.prettyUri(destination);
-  buffer.write('Compiled $sourceName to $destination.');
+  var destinationName = p.prettyUri(destination);
+  buffer.write('Compiled $sourceName to $destinationName.');
   if (options.color) buffer.write('\u001b[0m');
   print(buffer);
 }
