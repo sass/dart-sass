4605826708af8f6d5fe69fc7f842ddf8b938c13e
Natalie Weizenbaum <nweiz@google.com>
2018-10-10 18:05:32 -0700

    Properly record old members in stack frames

--- lib/src/visitor/evaluate.dart
+++ lib/src/visitor/evaluate.dart
@@ -1776,7 +1776,7 @@ class _EvaluateVisitor
   ///
   /// Runs [callback] with the new stack.
   T _withStackFrame<T>(String member, FileSpan span, T callback()) {
-    _stack.add(new Tuple2(member, span));
+    _stack.add(new Tuple2(_member, span));
     var oldMember = _member;
     _member = member;
     var result = callback();
