e3a5eed4dac8552336d27a42b11c97125c738b51
Natalie Weizenbaum <nweiz@google.com>
2019-02-13 13:59:44 -0800

    Revert "Run tests against sass-spec's feature.use branch"
    This reverts commit 4d23acd11948294f6d3e5c878c28eb728d67998c.

--- tool/travis/sass-spec-ref.sh
+++ tool/travis/sass-spec-ref.sh
@@ -7,7 +7,7 @@
 # run. If we're running specs for a pull request which refers to a sass-spec
 # pull request, we'll run against the latter rather than sass-spec master.
 
-default=feature.use
+default=master
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   >&2 echo "TRAVIS_PULL_REQUEST: $TRAVIS_PULL_REQUEST."
