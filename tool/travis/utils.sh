874c225968066e81d3a4f9bd8dafb14299d345b6
Natalie Weizenbaum <nweiz@google.com>
2019-05-24 13:37:18 -0700

    Get npm and pub credentials from environment variables
    Getting them from a file made the process of updating either set of
    credentials convoluted and error-prone, and meant that if one needed
    to be updated both had to be re-encrypted.
    
    This also updates the pub credentials to accommodate the new scope, as
    described in dart-lang/pub-dev#2281.

--- tool/travis/utils.sh
+++ tool/travis/utils.sh
@@ -2,15 +2,6 @@
 # license that can be found in the LICENSE file or at
 # https://opensource.org/licenses/MIT.
 
-# Decrypts the encrypted credentials using Travis's private key and saves them
-# to credentials.tar.
-function decrypt_credentials() {
-    openssl aes-256-cbc -K $encrypted_b4541d8c554c_key \
-            -iv $encrypted_b4541d8c554c_iv \
-            -in tool/travis/credentials.tar.enc \
-            -out credentials.tar -d
-}
-
 # Prints the invocation of a command and then runs that command, in the same way
 # Travis's internal infrastructure does.
 function travis_cmd() {
