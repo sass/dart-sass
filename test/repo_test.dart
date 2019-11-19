7e0baa160828496758a8d918c7a2a5658c0d73f8
Natalie Weizenbaum <nweiz@google.com>
2019-11-19 13:41:20 -0800

    reformat

--- test/repo_test.dart
+++ test/repo_test.dart
@@ -13,7 +13,8 @@ import 'package:yaml/yaml.dart';
 void main() {
   group("YAML files are valid:", () {
     for (var entry in Directory.current.listSync()) {
-      if (entry is File && (entry.path.endsWith(".yaml") || entry.path.endsWith(".yml"))) {
+      if (entry is File &&
+          (entry.path.endsWith(".yaml") || entry.path.endsWith(".yml"))) {
         test(p.basename(entry.path), () {
           // Shouldn't throw an error.
           loadYaml(entry.readAsStringSync());
