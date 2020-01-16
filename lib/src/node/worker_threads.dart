6da563068dc3396c132968118fd53e2cd0f352d2
Bee <pizzaman8099@gmail.com>
2020-01-15 18:10:31 -0800

    Setup initial import of 'worker_threads' node library

--- lib/src/node/worker_threads.dart
+++ lib/src/node/worker_threads.dart
@@ -1,4 +1,4 @@
-// Copyright 2018 Google Inc. Use of this source code is governed by an
+// Copyright 2020 Google Inc. Use of this source code is governed by an
 // MIT-style license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 import 'package:js/js.dart';
