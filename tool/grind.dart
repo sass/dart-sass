c1cdabc4a32cac488b4d27e1591ab8c6493bb650
Natalie Weizenbaum <nweiz@google.com>
2021-01-08 14:44:44 -0800

    Use GH_* instead of GITHUB_* for environment variables
    According to [the docs], environment variables may not begin with
    `GITHUB_`.
    
    [the docs]: https://docs.github.com/en/free-pro-team@latest/actions/reference/environment-variables#naming-conventions-for-environment-variables

--- tool/grind.dart
+++ tool/grind.dart
@@ -33,6 +33,8 @@ void main(List<String> args) {
           as Map<String, Object>;
   pkg.npmReadme.fn = () => _readAndResolveMarkdown("package/README.npm.md");
   pkg.standaloneName.value = "dart-sass";
+  pkg.githubUser.value = Platform.environment["GH_USER"];
+  pkg.githubPassword.value = Platform.environment["GH_TOKEN"];
 
   pkg.githubReleaseNotes.fn = () =>
       "To install Sass ${pkg.version}, download one of the packages below "
