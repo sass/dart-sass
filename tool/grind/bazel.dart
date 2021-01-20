27c899e77b3865a3ddd3ad4a5f949ed3e80794de
Natalie Weizenbaum <nweiz@google.com>
2021-01-20 15:55:51 -0800

    Wait longer before running the Bazel task. (#1212)
    This is still consistently failing to see the newly-uploaded npm
    package.

--- tool/grind/bazel.dart
+++ tool/grind/bazel.dart
@@ -29,7 +29,7 @@ Future<void> updateBazel() async {
   } on ProcessException catch (error) {
     if (error.stderr.contains("Couldn't find any versions for \"sass\"")) {
       log("The new sass version doesn't seem to be available yet, waiting 30s...");
-      await Future<void>.delayed(Duration(seconds: 30));
+      await Future<void>.delayed(Duration(minutes: 2));
       run("yarn", workingDirectory: p.join(repo, "sass"));
     }
   }
