c919db73aad44fd8cc12218bf0bb8b77573dec88
Natalie Weizenbaum <nweiz@google.com>
2021-06-16 13:36:56 -0700

    Support CompileRequest.quiet_deps and .verbose (#48)
    Closes #44

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,7 @@
 
 * Support version 1.0.0-beta.11 of the Sass embedded protocol:
   * Support `VersionRequest` and `VersionResponse`.
+  * Support `CompileRequest.quiet_deps` and `.verbose`.
   * Set `CanonicalizeRequest.from_import`.
 
 * Properly throw errors for range checks for colors.
--- bin/dart_sass_embedded.dart
+++ bin/dart_sass_embedded.dart
@@ -67,6 +67,8 @@ void main(List<String> args) {
               syntax: syntaxToSyntax(input.syntax),
               style: style,
               url: input.url.isEmpty ? null : input.url,
+              quietDeps: request.quietDeps,
+              verbose: request.verbose,
               sourceMap: sourceMapCallback);
           break;
 
@@ -78,6 +80,8 @@ void main(List<String> args) {
                 importers: importers,
                 functions: globalFunctions,
                 style: style,
+                quietDeps: request.quietDeps,
+                verbose: request.verbose,
                 sourceMap: sourceMapCallback);
           } on FileSystemException catch (error) {
             return OutboundMessage_CompileResponse()
