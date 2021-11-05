9cb976e1956efbc13b35790518f0b5dabe3204bc
awjin <awjinahn@gmail.com>
2021-11-04 17:00:55 -0700

    Static analysis.

--- lib/src/parse/keyframe_selector.dart
+++ lib/src/parse/keyframe_selector.dart
@@ -63,7 +63,9 @@ class KeyframeSelectorParser extends Parser {
       if (next == $plus || next == $minus) {
         buffer.writeCharCode(scanner.readChar());
       }
-      if (!isDigit(scanner.peekChar())) scanner.error("Expected digit.");
+      if (!isDigit(scanner.peekChar())) {
+        scanner.error("Expected digit.");
+      }
 
       while (isDigit(scanner.peekChar())) {
         buffer.writeCharCode(scanner.readChar());
