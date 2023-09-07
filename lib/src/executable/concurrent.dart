e95080e4f807c31eba229d02753d9de77c1e47a5
なつき <i@ntk.me>
2023-09-06 20:01:05 -0700

    Remove unnecessary imports

--- lib/src/executable/concurrent.dart
+++ lib/src/executable/concurrent.dart
@@ -2,7 +2,6 @@
 // MIT-style license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
-import 'dart:async';
 import 'dart:math' as math;
 
 import '../io.dart';
