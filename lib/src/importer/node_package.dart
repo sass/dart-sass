d24da96a22cdc893aeb269455e60759d4e2dc62e
James Stuckey Weber <jamesnw@gmail.com>
2023-11-06 16:34:55 -0500

    Revert to resolvePackageRoot working on Posix

--- lib/src/importer/node_package.dart
+++ lib/src/importer/node_package.dart
@@ -98,12 +98,10 @@ class NodePackageImporterInternal extends Importer {
   // absolute URL to the root directory for the most proximate installed
   // `packageName`.
   Uri? _resolvePackageRoot(String packageName, Uri baseURL) {
-    var baseDirectory = p.dirname(Uri.file(baseURL.toString()).toFilePath());
-    print("baseDirectory: $baseDirectory, baseURL: $baseURL");
+    var baseDirectory = p.dirname(baseURL.toFilePath());
+
     Uri? recurseUpFrom(String entry) {
-      if (!isWindows && !entry.startsWith(p.separator)) {
-        entry = "${p.separator}$entry";
-      }
+      if (!entry.startsWith("/")) entry = "/$entry";
       var potentialPackage = p.joinAll([entry, 'node_modules', packageName]);
       print("potentialPackage: $potentialPackage");
       if (dirExists(potentialPackage)) {
