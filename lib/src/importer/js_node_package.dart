397b78910dd43f244a7bb28b0fbd35c956a68b3b
Jonny Gerig Meyer <jonny@oddbird.net>
2024-01-05 14:50:38 -0500

    Add missing file.

---
+++ lib/src/importer/js_node_package.dart
@@ -0,0 +1,26 @@
+// Copyright 2023 Google Inc. Use of this source code is governed by an
+// MIT-style license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import 'package:node_interop/js.dart';
+
+import '../js/utils.dart';
+
+/// The JS [NodePackageImporter] class that can be added to the
+/// `importers` option to enable loading `pkg:` URLs from `node_modules`.
+class JSNodePackageImporter {
+  final String? entryPointPath;
+
+  JSNodePackageImporter(this.entryPointPath);
+}
+
+Never throwMissingFileSystem() {
+  jsThrow(JsError(
+      "The Node package importer cannot be used without a filesystem."));
+}
+
+Never throwMissingEntryPointPath() {
+  jsThrow(JsError("The Node package importer cannot determine an entry point "
+      "because `require.main.filename` is not defined. "
+      "Please provide an `entryPointPath` to the `NodePackageImporter`."));
+}
