069b2721fd427a495ad2dd695998c22b7ef8bc27
Jonny Gerig Meyer <jonny@oddbird.net>
2024-01-05 15:52:21 -0500

    update copyrights

--- lib/src/importer/js_node_package.dart
+++ lib/src/importer/js_node_package.dart
@@ -1,4 +1,4 @@
-// Copyright 2023 Google Inc. Use of this source code is governed by an
+// Copyright 2024 Google Inc. Use of this source code is governed by an
 // MIT-style license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
