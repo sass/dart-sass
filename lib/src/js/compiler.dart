471f638028d72cea22f70518a50d0c369817defb
Jonny Gerig Meyer <jonny@oddbird.net>
2024-01-08 14:52:54 -0500

    add missing file header

--- lib/src/js/compiler.dart
+++ lib/src/js/compiler.dart
@@ -1,3 +1,7 @@
+// Copyright 2024 Google LLC. Use of this source code is governed by an
+// MIT-style license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
 import 'dart:js_util';
 
 import 'package:async/async.dart';
