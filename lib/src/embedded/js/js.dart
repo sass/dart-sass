f1e95db4f6089f58cecb98bbcf787a2bffdbc419
なつき <i@ntk.me>
2024-10-27 23:35:08 -0700

    Implement `sass --embedded` in pure JS mode

---
+++ lib/src/embedded/js/js.dart
@@ -0,0 +1,15 @@
+// Copyright 2024 Google Inc. Use of this source code is governed by an
+// MIT-style license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import 'dart:js_interop';
+
+extension JSTypedArrayExtension on JSTypedArray {
+  external JSArrayBuffer get buffer;
+}
+
+extension JSArrayExtension<T extends JSAny?> on JSArray<T> {
+  external int get length;
+  external T operator [](int index);
+  external JSArray<T> slice([int start, int end]);
+}
