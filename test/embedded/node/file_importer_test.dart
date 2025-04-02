a8f5d2b59a87b1907839916b60804ce2b33764f5
なつき <i@ntk.me>
2025-04-01 18:44:52 -0700

    Address review feedbacks

--- test/embedded/node/file_importer_test.dart
+++ test/embedded/node/file_importer_test.dart
@@ -1,4 +1,4 @@
-// Copyright 2024 Google LLC. Use of this source code is governed by an
+// Copyright 2025 Google LLC. Use of this source code is governed by an
 // MIT-style license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
