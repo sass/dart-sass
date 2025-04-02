23ac67d879a10cdbdba0e0ac1e34be26f10182ad
なつき <i@ntk.me>
2025-04-01 20:14:55 -0700

    Add a comment for gracefulShutdown

--- lib/src/embedded/vm/executable.dart
+++ lib/src/embedded/vm/executable.dart
@@ -12,6 +12,14 @@ import '../worker_dispatcher.dart';
 
 void main(List<String> args) {
   if (parseOptions(args)) {
+    // The option `gracefulShutdown: false` means exit the process immediately
+    // without waiting for child isolates to shutdown. Dart VM does not need to
+    // wait as it does not have race condition issues between communication and
+    // worker shutdown in NodeJS.
+    //
+    // Because `gracefulShutdown` only gracefully terminates the worker that it
+    // does not wait for inflight requests to finish, there is no behavior
+    // difference to the end users.
     WorkerDispatcher(
             StreamChannel.withGuarantees(stdin, stdout, allowSinkErrors: false)
                 .transform(lengthDelimited),
