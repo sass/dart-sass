03c14bb0b1ecee181bd3f19e6151b3e98b03863d
なつき <i@ntk.me>
2025-04-01 19:57:11 -0700

    Rename files and move things around

--- lib/src/embedded/executable.dart
+++ lib/src/embedded/executable.dart
@@ -1,21 +1,5 @@
-// Copyright 2019 Google Inc. Use of this source code is governed by an
+// Copyright 2025 Google Inc. Use of this source code is governed by an
 // MIT-style license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
-import 'dart:io';
-
-import 'package:stream_channel/stream_channel.dart';
-
-import 'isolate_dispatcher.dart';
-import 'options.dart';
-import 'util/length_delimited_transformer.dart';
-
-void main(List<String> args) {
-  if (parseOptions(args)) {
-    IsolateDispatcher(
-            StreamChannel.withGuarantees(stdin, stdout, allowSinkErrors: false)
-                .transform(lengthDelimited),
-            gracefulShutdown: false)
-        .listen();
-  }
-}
+export 'vm/executable.dart' if (dart.library.js) 'js/executable.dart';
