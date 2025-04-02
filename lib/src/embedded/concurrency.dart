199222504dabc2333d8e39e1705c18754c18c3f9
なつき <i@ntk.me>
2025-04-01 18:44:52 -0700

    Address review feedbacks

--- lib/src/embedded/concurrency.dart
+++ lib/src/embedded/concurrency.dart
@@ -1,4 +1,4 @@
-// Copyright 2024 Google Inc. Use of this source code is governed by an
+// Copyright 2025 Google Inc. Use of this source code is governed by an
 // MIT-style license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
@@ -6,5 +6,6 @@ import 'dart:ffi';
 
 /// More than MaxMutatorThreadCount isolates in the same isolate group
 /// can deadlock the Dart VM.
+///
 /// See https://github.com/sass/dart-sass/pull/2019
 int get concurrencyLimit => sizeOf<IntPtr>() <= 4 ? 7 : 15;
