4a8dc7e88bfc6715adbf03d19dff0f87890c17f5
Carlos <goodwine@google.com>
2026-01-15 01:08:52 +0000

    update eslint config to match gts 7.0.0 new requirements
    - installed new dependencies for new eslint
    - updated ts eslint config
    - migrated eslint config with @eslint/migrate-config

---
+++ pkg/sass-parser/eslint.config.mjs
@@ -0,0 +1,34 @@
+import {defineConfig, globalIgnores} from 'eslint/config';
+import gts from 'gts';
+
+export default defineConfig([
+  globalIgnores(['**/dist/', '**/*.js']),
+  {
+    extends: [...gts],
+
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': [
+        'error',
+        {
+          allowExpressions: true,
+        },
+      ],
+    },
+
+    'func-style': ['error', 'declaration'],
+
+    'prefer-const': [
+      'error',
+      {
+        destructuring: 'all',
+      },
+    ],
+
+    'sort-imports': [
+      'error',
+      {
+        ignoreDeclarationSort: true,
+      },
+    ],
+  },
+]);
