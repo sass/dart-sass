91831b839bd785c97f51445e9ae0a560bc010d74
Natalie Weizenbaum <nweiz@google.com>
2026-08-17 13:42:51 -0700

    Fix dartdoc error

--- lib/src/importer/package.dart
+++ lib/src/importer/package.dart
@@ -16,8 +16,8 @@ class PackageImporter(
   final PackageConfig _packageConfig,
 ) extends Importer {
   /// Creates an importer that loads stylesheets from `package:` URLs according
-  /// to [packageConfig], which is a [PackageConfig][] from the `package_config`
-  /// package.
+  /// to [_packageConfig], which is a [PackageConfig][] from the
+  /// `package_config` package.
   ///
   /// [`PackageConfig`]: https://pub.dev/documentation/package_config/latest/package_config.package_config/PackageConfig-class.html
   this;
